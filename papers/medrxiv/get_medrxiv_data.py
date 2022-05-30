46cc7101434ee247a4e8669b4c0e49ccd3959399
Sourcery AI <>
2022-05-30 17:57:26 +0000

    'Refactored by Sourcery'

--- papers/medrxiv/get_medrxiv_data.py
+++ papers/medrxiv/get_medrxiv_data.py
@@ -5,26 +5,25 @@ from bs4 import BeautifulSoup
 
 
 def medrxiv_data(keyword: str) -> list:
-    url = "https://www.medrxiv.org/search/%s" % keyword
+    url = f"https://www.medrxiv.org/search/{keyword}"
     res = requests.get(url)
     soup = BeautifulSoup(res.text, "lxml")
     titles = soup.select("div > span > a")
 
     data_list = []
     for title in titles:
-        dic = {}
-        dic["link"] = "https://www.medrxiv.org" + title.get("href")
+        dic = {"link": "https://www.medrxiv.org" + title.get("href")}
         dic["title"] = title.span.text
         dic["time"] = str(title.get("href")).split(
             "/")[-1][:10].replace('.', "")
         data_list.append(dic)
 
         # 下载
-        file_name = "{}.pdf".format(dic["title"])
+        file_name = f'{dic["title"]}.pdf'
         if (file_name not in os.listdir("./paper_pdf")) & (int(dic["time"]) > 20200101):
             pdf_url = dic["link"] + ".full.pdf"
             r = requests.get(pdf_url)
-            with open("./paper_pdf/%s" % file_name, 'wb') as f:
+            with open(f"./paper_pdf/{file_name}", 'wb') as f:
                 f.write(r.content)
 
     return data_list
@@ -35,7 +34,7 @@ def get_list(key_list: list) -> list:
     for i in key_list:
         once_data = medrxiv_data(i)
         data_list.extend(once_data)
-    data_list = [dict(t) for t in set([tuple(d.items()) for d in data_list])]
+    data_list = [dict(t) for t in {tuple(d.items()) for d in data_list}]
     return data_list
 
 
